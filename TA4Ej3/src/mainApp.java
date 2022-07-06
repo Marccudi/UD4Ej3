05b60b63af95ee94c618fcc9e6fead13dd725d22
Marccudi <marccudi@hotmail.es>
2022-07-06 22:08:14 +0200

    Ejercicio 3 acabado

---
+++ TA4Ej3/src/mainApp.java
@@ -0,0 +1,29 @@
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 35;
+		int y = 14;
+		double n = 35.14;
+		double m = 14.35;
+		System.out.println("x= "+x+" y= "+y+" n= "+n+" m= "+m);
+		System.out.println("x + y = " + (x+y));
+		System.out.println("x - y = " + (x-y));
+		System.out.println("x * y = " + (x*y));
+		System.out.println("x / y = " + (x/y));
+		System.out.println("x % y = " + (x%y));		
+		System.out.println("n + m = " + (n+m));
+		System.out.println("n - m = " + (n-m));
+		System.out.println("n * m = " + (n*m));
+		System.out.println("n / m = " + (n/m));
+		System.out.println("n % m = " + (n%m));
+		System.out.println("x + n = " + (n+x));
+		System.out.println("y / m = " + (y/m));
+		System.out.println("y % m = " + (y%m));
+		System.out.println("x * 2 = "+(x*2)+" y * 2 = "+(y*2)+" n * 2 = "+(n*2)+" m * 2 = "+(m*2));
+		System.out.println("x + y + n + m = "+(x+y+n+m));
+		System.out.println("x * y * n * m = "+(x*y*n*m));
+	}
+
+}
